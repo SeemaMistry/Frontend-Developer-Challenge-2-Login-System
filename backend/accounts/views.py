6b8456ed77dd81f216b772ec2fd2afbc4c2fbd6e
SeemaMistry <seema.m.1996@gmail.com>
2023-11-23 18:06:54 -0500

    feat: get up imports and view classes with csrfToken cookie method decorators

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class CheckAuthenticated(APIView):
+
+    def get(self, request, format=None):
+        pass
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class RegisterView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self, request, format=None):
+        pass
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class LoginView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self, request, format=None):
+        pass
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class LogoutView(APIView):
+
+    def post(self, request, format=None):
+        pass
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class GetCSRFTokenView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def get(self, request, format=None):
+
