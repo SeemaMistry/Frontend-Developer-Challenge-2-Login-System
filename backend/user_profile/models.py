8c200eb0619e9a3b87051b544b6fcd5d058f3cb4
SeemaMistry <seema.m.1996@gmail.com>
2023-11-24 18:16:18 -0500

    feat: create UserProfile model with all fields for prepr lab assignment

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # set fields
+    first_name = models.CharField(max_length=225, default='')
+    last_name = models.CharField(max_length=225, default='')
+    username = models.CharField(max_length=20, default='')
+    email = models.EmailField(max_length=225, default='')
+    userType = models.CharField(max_length=50, default='')
+    language = models.CharField(max_length=25, default='')
+
+    def __str__(self):
+        return self.first_name + self.last_name
