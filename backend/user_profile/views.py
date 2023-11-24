574071a7d55f01910ea03f15d4df80a315696a69
SeemaMistry <seema.m.1996@gmail.com>
2023-11-24 18:53:31 -0500

    feat: create GetUserProfile view to retrieve and pass user_profile data in response

--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import UserProfile
+from .serializers import UserProfileSerializer
 
-# Create your views here.
+class GetUserProfile(APIView):
+    def get(self, request, format=None):
+        # get current user
+        user = self.request.user
+        try:
+            # obtain user profile of user and pass it in response
+            user_profile = UserProfile.objects.get(user=user)
+            user_profile = UserProfileSerializer(user_profile)
+            return Response({'profile': user_profile.data})
+        except:
+            return Response({'error': 'Something went wrong when retrieving user profile'})
+
