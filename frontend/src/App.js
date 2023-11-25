2c4be7e2d978b213e38ec2fd23d783800fce593b
SeemaMistry <seema.m.1996@gmail.com>
2023-11-25 14:41:55 -0500

    feat: create redux store and pass store in Provider tag in root App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import './App.css'
+
 import Layout from './hocs/Layout'
 import Login from './containers/Login'
 import Register from './containers/Register'
@@ -8,15 +11,17 @@ import User from './containers/User'
 
 const App = () => {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route exact path='/login' Component={Login}></Route>
-          <Route exact path='/register' Component={Register}></Route>
-          <Route exact path='/user' Component={User}></Route>
-        </Routes>
-      </Layout>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path='/login' Component={Login}></Route>
+            <Route exact path='/register' Component={Register}></Route>
+            <Route exact path='/user' Component={User}></Route>
+          </Routes>
+        </Layout>
+      </Router>
+    </Provider>
   );
 }
 
