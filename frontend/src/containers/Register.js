8a31eda71988f3f6e1283ff1ab17f14998725907
SeemaMistry <seema.m.1996@gmail.com>
2023-11-24 23:04:46 -0500

    feat: create a simple register form with username, password and re_password

--- frontend/src/containers/Register.js
+++ frontend/src/containers/Register.js
@@ -1,8 +1,62 @@
 import React from 'react'
+import {Link, Navigate} from 'react-router-dom'
 
 const Register = () => {
+  const accountCreated = false
+  const onChange = e => {}
+
+  const onSubmit = e => {}
+
   return (
-    <div>Register</div>
+    <div className='container mt-5'>
+      {accountCreated && <Navigate to='/login' replace/>}
+      <h1>Register for an Account</h1>
+      <p>Create an account with PreprLabs</p>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label className='form-label mt-3'>Username: </label>
+          <input
+            className='form-control'
+            type='text'
+            placeholder='username'
+            onChange={onChange}
+            // value={username}
+            name='username'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label className='form-label mt-3'>Password: </label>
+          <input
+            className='form-control'
+            type='password'
+            placeholder='password'
+            onChange={onChange}
+            // value={password}
+            name='password'
+            minLength='6'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label className='form-label mt-3'>Retype Password: </label>
+          <input
+            className='form-control'
+            type='password'
+            placeholder='confirm password'
+            onChange={onChange}
+            // value={re_password}
+            name='re_password'
+            minLength='6'
+            required
+          />
+        </div>
+        <button className='btn btn-primary mt-3' type='submit'>Register</button>
+      </form>
+      <p className='mt-3'>
+        Already have an account? <Link to={'/login'}>Sign In</Link>
+      </p>
+    </div>
   )
 }
 
