784f9d1fa82cd04555a8765ab0180f4162e9e20d
SeemaMistry <seema.m.1996@gmail.com>
2023-11-25 16:55:41 -0500

    feat: use connect to retireve state.profile user data and render user data onto the User page

--- frontend/src/containers/User.js
+++ frontend/src/containers/User.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-const User = () => {
+const User = ({
+  first_name,
+  last_name,
+  username,
+  email,
+  user_type,
+  language
+
+}) => {
+
   return (
-    <div>User</div>
+    <div>
+      <h1>User Info:</h1>
+      <p><bold>First Name: </bold>{first_name}</p>
+      <p><bold>Last Name: </bold>{last_name}</p>
+      <p><bold>Username: </bold>{username}</p>
+      <p><bold>Email: </bold>{email}</p>
+      <p><bold>User Type: </bold>{user_type}</p>
+      <p><bold>Language: </bold>{language}</p>
+      
+    </div>
   )
 }
 
-export default User
+const mapStateToProps = state => ({
+  first_name: state.profile.first_name,
+  last_name: state.profile.last_name,
+  username: state.profile.username,
+  email: state.profile.email,
+  user_type: state.profile.user_type,
+  language: state.profile.language 
+})
+export default connect(mapStateToProps, null)(User)
