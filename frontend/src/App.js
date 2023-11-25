6377f07680a2de376fd575642203be3ceedba2e5
SeemaMistry <seema.m.1996@gmail.com>
2023-11-24 22:43:14 -0500

    feat: set background color to light green

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
+import './App.css'
 import Layout from './hocs/Layout'
 import Login from './containers/Login'
 import Register from './containers/Register'
