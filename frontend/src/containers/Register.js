7c8b14817312a20eaf85d9069cb54a627918a188
SeemaMistry <seema.m.1996@gmail.com>
2023-11-25 21:23:43 -0500

    fix: extract password, re_password from formData and create value-state, validator-state, and focus-state with useEffect handling password validation as user types, and conditionally rendered error messages

--- frontend/src/containers/Register.js
+++ frontend/src/containers/Register.js
@@ -1,8 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import {Link, Navigate} from 'react-router-dom'
 import {connect} from 'react-redux'
 import { register } from '../actions/auth'
 import CSRFToken from '../components/CSRFToken'
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,14}$/;
+// pass#orD50
 
 const Register = ({register}) => {
   // set formData state
@@ -10,26 +14,51 @@ const Register = ({register}) => {
     first_name: '',
     last_name: '',
     username: '',
-    password: '',
-    re_password: '',
+    // password: '',
+    // re_password: '',
     email: '',
     user_type: '',
     language: ''
   })
 
   const [accountCreated, setAccountCreated] = useState(false) // redirect to login page if successful account created
-  const {first_name, last_name, username, password, re_password, email, user_type, language} = formData // destructure forData
+  // const {first_name, last_name, username, password, re_password, email, user_type, language} = formData // destructure forData
+  const {first_name, last_name, username, email, user_type, language} = formData
 
+  const [pwd, setPwd] = useState('');
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState('');
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidMatch(pwd === matchPwd);
+}, [pwd, matchPwd])
+
+
+  const onChangeUsername = e => setFormData({...formData, [e.target.name]: e.target.value.replace(/[^\w\s]/gi, "")})
   const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
 
   const onSubmit = e => {
     e.preventDefault()
+    register(first_name, last_name, username, pwd, matchPwd, email, user_type, language)
+    setAccountCreated(true)
+
      // validate user-inputs for registration
-     if (password === re_password) {
-      register(first_name, last_name, username, password, re_password, email, user_type, language)
-      setAccountCreated(true)
-    } 
+    //  if (password === re_password) {
+    //   register(first_name, last_name, username, password, re_password, email, user_type, language)
+    //   setAccountCreated(true)
+    // } 
+    
   }
+
+
+
+
 
   return (
     <div className='container mt-5'>
@@ -65,10 +94,11 @@ const Register = ({register}) => {
         <div className='form-group'>
           <label className='form-label mt-3'>Username: </label>
           <input
+           
             className='form-control'
             type='text'
             placeholder='Username'
-            onChange={onChange}
+            onChange={onChangeUsername}
             value={username}
             name='username'
             required
@@ -77,15 +107,27 @@ const Register = ({register}) => {
         <div className='form-group'>
           <label className='form-label mt-3'>Password: </label>
           <input
+          
             className='form-control'
             type='password'
             placeholder='Password'
-            onChange={onChange}
-            value={password}
+            onChange={(e) => setPwd(e.target.value)}
+            aria-invalid={validPwd ? "false" : "true"}
+            onFocus={() => setPwdFocus(true)}
+            onBlur={() => setPwdFocus(false)}
+            value={pwd}
             name='password'
             minLength='6'
             required
-          />
+          /> 
+          {pwdFocus && !validPwd ? 
+                <p>
+                  8 to 24 characters.<br />
+                  Must include uppercase and lowercase letters, a number and a special character.<br />
+                  Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                </p>
+              : <p></p>
+        }
         </div>
         <div className='form-group'>
           <label className='form-label mt-3'>Retype Password: </label>
@@ -93,12 +135,18 @@ const Register = ({register}) => {
             className='form-control'
             type='password'
             placeholder='Confirm password'
-            onChange={onChange}
-            value={re_password}
+            onChange={(e) => setMatchPwd(e.target.value)}
+            value={matchPwd}
             name='re_password'
             minLength='6'
             required
+            aria-invalid={validMatch ? "false" : "true"}
+            aria-describedby="confirmnote"
+            onFocus={() => setMatchFocus(true)}
+            onBlur={() => setMatchFocus(false)}
           />
+          {matchFocus && !validMatch ? <p>Must match the first password input field.</p> : <p></p>}
+
         </div>
         <div className='form-group'>
           <label className='form-label mt-3'>Email: </label>
